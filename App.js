97d45657b0a6576a494987d4299080c424ad7dd6
Konstantopaidos-Emmanouil <97766441+Konstantopaidos-Emmanouil@users.noreply.github.com>
2022-07-02 19:30:45 +0200

    First commit Message

---
+++ App.js
@@ -0,0 +1,42 @@
+import { StyleSheet, ImageBackground, SafeAreaView } from "react-native";
+import StartGameScreen from "./screens/StartGameScreen";
+import { LinearGradient } from "expo-linear-gradient";
+import { useState } from "react";
+import GameScreen from "./screens/GameScreen";
+import Colors from "./constants/colors";
+
+export default function App() {
+  const [userNumber, setUserNumber] = useState();
+
+  function pickedNumberHandler(pickedNumber) {
+    setUserNumber(pickedNumber);
+  }
+
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
+
+  if (userNumber) {
+    screen = <GameScreen userNumber={userNumber}/>;
+  }
+
+  return (
+    <LinearGradient colors={[Colors.primary700, Colors.accent500]} style={styles.rootScreen}>
+      <ImageBackground
+        source={require("./assets/baackground.png")}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backGroungImage}
+      >
+        <SafeAreaView style={styles.rootScreen} >{screen}</SafeAreaView>
+      </ImageBackground>
+    </LinearGradient>
+  );
+}
+
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  },
+  backGroungImage: {
+    opacity: 0.15,
+  },
+});
